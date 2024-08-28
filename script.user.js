5092c943158be4e319944b35375fa8baee69e96b
6a67 <83820531+6a67@users.noreply.github.com>
2024-08-28 22:38:28 +0200

    simplify github action

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name JPDB Userscript (6a67)
 // @namespace http://tampermonkey.net/
-// @version 0.1.105
+// @version 0.1.106
 // @description Script for JPDB that adds some styling and functionality
 // @match https://jpdb.io/*
 // @grant GM_addStyle
