aa99dfa8e2999b148246c0c95800d5306c3a9994
6a67 <83820531+6a67@users.noreply.github.com>
2025-01-02 07:09:01 +0100

    reorder font and style injection in initialization sequence

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name JPDB Userscript (6a67)
 // @namespace http://tampermonkey.net/
-// @version 0.1.186
+// @version 0.1.187
 // @description Script for JPDB that adds some styling and functionality
 // @match *://jpdb.io/*
 // @grant GM_addStyle
@@ -4326,8 +4326,8 @@
     }
 
     function init() {
-        injectFont();
         applyStyles();
+        injectFont();
         if (window.location.href === CONFIG.learnPageUrl) {
             initLearnPage();
             // initInjectStatsIntoLearnPage();
